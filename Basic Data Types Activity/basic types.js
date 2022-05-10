e9f2aef79bf465ef59f1f094ed46ebca0edad3f7
PossibeHacker <78627876+PossibeHacker@users.noreply.github.com>
2022-05-09 22:18:06 -0700

    Add files via upload

---
+++ Basic Data Types Activity/basic types.js
@@ -0,0 +1,19 @@
+let greetings = "welcome to my space ship";
+let name = "Baron b berry";
+let title = "incompentent captain";
+let worldName = "the void";
+
+let century = 40;
+let shipNumber = 60;
+let myAge = 70;
+
+let shipType = ["cruiser","destroyer","carrier","frigate"];
+
+let hasCapitalShip = true;
+
+console.log(greetings + " my name is " + name + ".");
+console.log("I'm the " + title + " of this ship.");
+console.log("As you can see it's " + hasCapitalShip + " that I have a " + shipType[3]);
+console.log("In the " + century + "'th century people live longer so I look like i'm 40 but i'm really " + myAge);
+console.log("this is ship number " + shipNumber + " this is because it was the " + shipNumber + "th ship of this type built");
+console.log("welcome to the crew, have fun.");
